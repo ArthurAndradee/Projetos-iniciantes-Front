689d76b2ba1ed4c3c1a7b62f4ca2a631c1e594e8
Arthur <121949480+ArthurAndradee@users.noreply.github.com>
2023-01-15 09:19:11 -0300

    Add files via upload

---
+++ Javascript/Javascript básico/String.js
@@ -0,0 +1,12 @@
+var fullname="Arthur Andrade da Silva";
+console.log (fullname.length)
+
+console.log (fullname.length + 4 )
+console.log (fullname.length - 5)
+console.log (fullname.length * 6)
+console.log (fullname.length / 2)
+
+var fullname="Arthur Andrade da Silva";
+var fullnamelength = "23";
+console.log(fullname[fullnamelength - 5]);
+
