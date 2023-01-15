87091a0cbab5c0911298c6d22370d3340a16cfd2
Arthur <121949480+ArthurAndradee@users.noreply.github.com>
2023-01-15 00:29:26 -0300

    Add files via upload

---
+++ Javascript/Javascript básico/Números.js
@@ -0,0 +1,2 @@
+let value1 = Math.floor(Math.random() * 60);
+console.log (value1)
---
+++ Javascript/Javascript básico/Variáveis.js
@@ -0,0 +1,6 @@
+var name = "Arthur";
+{
+    let name = "Luana";
+    console.log("Dentro do bloco, o nome é: " + name);
+}
+console.log("Fora do bloco, o nome é: " + name);
