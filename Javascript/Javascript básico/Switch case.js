310b9ea3e27dbe4b934b0e56ea1a5ba716ec27af
Arthur <121949480+ArthurAndradee@users.noreply.github.com>
2023-01-16 09:44:43 -0300

    Add files via upload

---
+++ Javascript/Javascript básico/Switch case.js
@@ -0,0 +1,21 @@
+studentAge = 15
+
+switch(studentAge) {
+    case 8:
+    case 9:
+    case 10:
+        console.log ("Turma Infantil")
+        break;
+    case 11:
+    case 12:
+    case 13:
+        console.log ("Turma Juvenil")
+        break;
+    case 14:
+    case 15:
+    case 16:
+        console.log ("Turma Jovem Adulto")
+        break;
+    default:
+        console.log ("Entre em contato com a secretaria")
+}
